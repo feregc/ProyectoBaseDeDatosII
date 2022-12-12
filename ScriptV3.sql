eda9fb88e53c7ed93974160083200e1f09753c2c
Fernando Garcia <gfernandoentique37@gmail.com>
2022-12-11 20:37:35 -0600

    Nueva Version del Script y del Diagrama

--- ScriptV3.sql
+++ ScriptV3.sql
@@ -10,12 +10,24 @@ CREATE TABLE dimCountryRegion(
 );
 GO
 
+CREATE TABLE dimTerritory(
+	idTerritory INTEGER NOT NULL,
+	nameTerritory NVARCHAR(50) NOT NULL,
+	territoryGroup NVARCHAR(50) NOT NULL,
+	idCountryRegion NVARCHAR(3) NOT NULL,
+	CONSTRAINT PK_Territory PRIMARY KEY (idTerritory),
+	CONSTRAINT FK_Territory_CountryRegion FOREIGN KEY (idCountryRegion) REFERENCES dimCountryRegion(idCountryRegion)
+);
+GO
+
 CREATE TABLE dimStateProvince(
 	idStateProvince INTEGER NOT NULL,
 	nameStateProvince NVARCHAR(50) NOT NULL, 
 	idCountryRegion NVARCHAR(3) NOT NULL,
+	idTerritory INTEGER NOT NULL,
 	CONSTRAINT PK_StateProvince PRIMARY KEY (idStateProvince),
-	CONSTRAINT FK_StateProvince_CountryRegion FOREIGN KEY (idCountryRegion) REFERENCES dimCountryRegion(idCountryRegion)
+	CONSTRAINT FK_StateProvince_CountryRegion FOREIGN KEY (idCountryRegion) REFERENCES dimCountryRegion(idCountryRegion),
+	CONSTRAINT FK_StateProvince_Territory FOREIGN KEY (idTerritory) REFERENCES dimTerritory(idTerritory)
 );
 GO
 
@@ -40,12 +52,25 @@ CREATE TABLE dimCustomers(
 );
 GO
 
+CREATE TABLE dimEmployees(
+	idEmployee INTEGER NOT NULL,
+	dni NVARCHAR(15) NOT NULL,
+	firstName NVARCHAR(50) NOT NULL,
+	lastName NVARCHAR(50) NOT NULL,
+	gender NCHAR(1) CHECK (gender IN ('F','M')),
+	hireDate DATE NOT NULL,
+	CONSTRAINT PK_Employee PRIMARY KEY (idEmployee)
+);
+GO
+
 CREATE TABLE dimStores(
 	idStore INTEGER NOT NULL,
 	nameStore NVARCHAR(50) NOT NULL,
 	idLocation INTEGER NOT NULL,
+	idEmployee INTEGER NOT NULL,
 	CONSTRAINT PK_Stores PRIMARY KEY (idStore),
-	CONSTRAINT FK_Stores_Location FOREIGN KEY (idLocation) REFERENCES dimLocations(idLocation)
+	CONSTRAINT FK_Stores_Location FOREIGN KEY (idLocation) REFERENCES dimLocations(idLocation),
+	CONSTRAINT FK_Stores_Employee FOREIGN KEY (idEmployee) REFERENCES dimEmployees(idEmployee)
 );
 GO
 
@@ -56,51 +81,113 @@ CREATE TABLE dimProductCategory(
 );
 GO
 
+CREATE TABLE dimProductSubCategory(
+	idProductSubCategory INTEGER NOT NULL,
+	nameProductSubCategory NVARCHAR(50) NOT NULL,
+	idProductCategory INTEGER NOT NULL,
+	CONSTRAINT PK_ProductSubCategory PRIMARY KEY (idProductSubCategory),
+	CONSTRAINT FK_ProductSubCategory_ProductCategory FOREIGN KEY (idProductCategory) REFERENCES dimProductCategory(idProductCategory)
+);
+
 CREATE TABLE dimProducts(
 	idProduct INTEGER NOT NULL,
 	nameProduct NVARCHAR(50) NOT NULL,
 	listPrice MONEY NOT NULL,
 	standarCost MONEY NOT NULL,
-	idProductCategory INTEGER NOT NULL,
+	idProductSubCategory INTEGER NOT NULL,
 	CONSTRAINT PK_Product PRIMARY KEY (idProduct),
-	CONSTRAINT FK_product_ProductCategory FOREIGN KEY (idProductCategory) REFERENCES dimProductCategory(idProductCategory)
+	CONSTRAINT FK_Product_ProductSubCategory FOREIGN KEY (idProductSubCategory) REFERENCES dimProductSubCategory(idProductSubCategory)
 );
 GO
 
-CREATE TABLE dimEmployees(
-	idEmployee INTEGER NOT NULL,
-	dni NVARCHAR(15) NOT NULL,
-	firstName NVARCHAR(50) NOT NULL,
-	lastName NVARCHAR(50) NOT NULL,
-	gender NCHAR(1) CHECK (gender IN ('F','M')),
-	CONSTRAINT PK_Employee PRIMARY KEY (idEmployee)
-);
-GO
+DECLARE @StartDate  date = '20150101';
 
-CREATE TABLE dimTime(
-	idTime INTEGER NOT NULL,
-	year DATE NOT NULL,
-	month DATE NOT NULL,
-	trimester DATE NOT NULL,
-	CONSTRAINT PK_Time PRIMARY KEY (idTime)
+DECLARE @CutoffDate date = DATEADD(DAY, -1, DATEADD(YEAR, 20, @StartDate));
+
+;WITH seq(n) AS 
+(
+  SELECT 0 UNION ALL SELECT n + 1 FROM seq
+  WHERE n < DATEDIFF(DAY, @StartDate, @CutoffDate)
+),
+d(d) AS 
+(
+  SELECT DATEADD(DAY, n, @StartDate) FROM seq
+),
+src AS
+(
+  SELECT
+    TheDate         = CONVERT(date, d),
+    TheDay          = DATEPART(DAY,       d),
+    TheDayName      = DATENAME(WEEKDAY,   d),
+    TheWeek         = DATEPART(WEEK,      d),
+    TheISOWeek      = DATEPART(ISO_WEEK,  d),
+    TheDayOfWeek    = DATEPART(WEEKDAY,   d),
+    TheMonth        = DATEPART(MONTH,     d),
+    TheMonthName    = DATENAME(MONTH,     d),
+    TheQuarter      = DATEPART(Quarter,   d),
+    TheYear         = DATEPART(YEAR,      d),
+    TheFirstOfMonth = DATEFROMPARTS(YEAR(d), MONTH(d), 1),
+    TheLastOfYear   = DATEFROMPARTS(YEAR(d), 12, 31),
+    TheDayOfYear    = DATEPART(DAYOFYEAR, d)
+  FROM d
+),
+dim AS
+(
+  SELECT
+    TheDate, 
+    TheDay,
+    TheDaySuffix        = CONVERT(char(2), CASE WHEN TheDay / 10 = 1 THEN 'th' ELSE 
+                            CASE RIGHT(TheDay, 1) WHEN '1' THEN 'st' WHEN '2' THEN 'nd' 
+                            WHEN '3' THEN 'rd' ELSE 'th' END END),
+    TheDayName,
+    TheDayOfWeek,
+   
+    TheWeek,
+    TheISOweek,
+   
+    TheMonthName,
+   
+    TheQuarter,
+    TheFirstOfQuarter   = MIN(TheDate) OVER (PARTITION BY TheYear, TheQuarter),
+    TheLastOfQuarter    = MAX(TheDate) OVER (PARTITION BY TheYear, TheQuarter),
+    TheYear,
+   
+    Style120            = CONVERT(char(10), TheDate, 120)
+  FROM src
+)
+
+SELECT * INTO dbo.dimDate FROM dim
+  ORDER BY TheDate
+  OPTION (MAXRECURSION 0);
+ALTER TABLE dimDate 
+ALTER COLUMN TheDate DATE NOT NULL;
+CREATE UNIQUE CLUSTERED INDEX PK_DateDimension ON dbo.dimDate(TheDate);
+
+CREATE TABLE dimSalesTaxes(
+	idSales INTEGER NOT NULL,
+	taxAmount MONEY NOT NULL,
+	freight MONEY NOT NULL,
+	CONSTRAINT PK_SalesTaxes PRIMARY KEY (idSales)
 );
 GO
 
 CREATE TABLE factSales(
-	idSales INTEGER IDENTITY NOT NULL,
+	idSalesDetail INTEGER NOT NULL,
+	idSales INTEGER NOT NULL,
 	idCustomer INTEGER NOT NULL,
 	idEmployee INTEGER NOT NULL,
-	idTime INTEGER NOT NULL,
-	idStore INTEGER NOT NULL,
+	idDate DATE NOT NULL,
+	idTerritory INTEGER NOT NULL,
 	idLocation INTEGER NOT NULL,
 	idProduct INTEGER NOT NULL,
-	quantity INTEGER NOT NULL,
-	total MONEY NOT NULL
-	CONSTRAINT PK_Sales PRIMARY KEY (idSales),
+	orderQuantity INTEGER NOT NULL,
+	unitPrice MONEY NOT NULL
+	CONSTRAINT PK_Sales PRIMARY KEY (idSalesDetail),
+	CONSTRAINT FK_Sales_SalesTaxes FOREIGN KEY (idSales) REFERENCES dimSalesTaxes(idSales),
 	CONSTRAINT FK_Sales_Customer FOREIGN KEY (idCustomer) REFERENCES dimCustomers(idCustomer),
 	CONSTRAINT FK_Sales_Employee FOREIGN KEY (idEmployee) REFERENCES dimEmployees(idEmployee),
-	CONSTRAINT FK_Sales_Time FOREIGN KEY (idTime) REFERENCES dimTime(idTime),
-	CONSTRAINT FK_Sales_Store FOREIGN KEY (idStore) REFERENCES dimStores(idStore),
+	CONSTRAINT FK_Sales_Date FOREIGN KEY (idDate) REFERENCES dimDate(TheDate),
+	CONSTRAINT FK_Sales_Territory FOREIGN KEY (idTerritory) REFERENCES dimTerritory(idTerritory),
 	CONSTRAINT FK_Sales_Location FOREIGN KEY (idLocation) REFERENCES dimLocations(idLocation),
 	CONSTRAINT FK_Sales_Product FOREIGN KEY (idProduct) REFERENCES dimProducts(idProduct)
 );
